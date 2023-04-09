592efe5afb3d7e9a8eb2d1755928c9997012ed1e
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-04-08 22:57:48 -0400

    V 2.0

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const adivinanzas = [
   {
-    pregunta: "Adivinanza 1: ¿Qué cosa es que al crecer se achica?",
-    respuesta: "un agujero",
+    pregunta: "Perteneciente al reino de los hongos (8 letras): M_ C _ _ _ _ A",
+    respuesta: "micotina",
+    mensaje: "Busca debajo del cajón de tus pantalones",
     html: `
-        <h2>Adivinanza 1</h2>
-        <p>¿Qué cosa es que al crecer se achica?</p>
+        <h2>Pregunta 1</h2>
+        <p>Perteneciente al reino de los hongos (8 letras </br>M _ C _ _ _ N A</p>
         <form>
           <input type="text" placeholder="Escribe tu respuesta">
-          <button type="submit">Enviar</button>
+          <button type="submit" id="botonEnviar">Enviar</button>
         </form>
       `,
   },
   {
-    pregunta: "Adivinanza 2: ¿Qué cosa es que mientras más grande menos se ve?",
-    respuesta: "oscuridad",
+    pregunta: "Adivinanza 2",
+    respuesta: "Respuesta 2",
+    mensaje: "Busca detras del trono del",
     html: `
         <h2>Adivinanza 2</h2>
-        <p>¿Qué cosa es que mientras más grande menos se ve?</p>
+        <p>Adivinanza 2</p>
         <form>
           <input type="text" placeholder="Escribe tu respuesta">
-          <button type="submit">Enviar</button>
+          <button type="submit" id="botonEnviar">Enviar</button>
         </form>
       `,
   },
   {
-    pregunta:
-      "Adivinanza 3: En el agua nado yo, en el agua me hundo yo, pero al fuego no me acerco yo. ¿Qué soy?",
-    respuesta: "el hielo",
+    pregunta: "Adivinanza 3",
+    respuesta: "Respuesta 3",
     html: `
         <h2>Adivinanza 3</h2>
-        <p>En el agua nado yo, en el agua me hundo yo, pero al fuego no me acerco yo. ¿Qué soy?</p>
+        <p>Adivinanza 3</p>
         <form>
           <input type="text" placeholder="Escribe tu respuesta">
-          <button type="submit">Enviar</button>
+          <button type="submit" id="botonEnviar">Enviar</button>
         </form>
       `,
   },
   {
-    pregunta:
-      "Adivinanza 4: Con letras tres te nombro, con una te añoro, y aunque no te pueda ver, eres la estrella que me guía. ¿Quién eres tú?",
-    respuesta: "Dios",
+    pregunta: "Adivinanza 4",
+    respuesta: "Respuesta 4",
     html: `
         <h2>Adivinanza 4</h2>
-        <p>Con letras tres te nombro, con una te añoro, y aunque no te pueda ver, eres la estrella que me guía. ¿Quién eres tú?</p>
+        <p>Adivinanza 4</p>
         <form>
           <input type="text" placeholder="Escribe tu respuesta">
-          <button type="submit">Enviar</button>
+          <button type="submit" id="botonEnviar">Enviar</button>
         </form>
       `,
   },
@@ -61,14 +61,33 @@ function mostrarAdivinanza(adivinanza) {
     event.preventDefault();
     const respuesta = event.target.querySelector("input").value.toLowerCase();
     if (respuesta === adivinanza.respuesta.toLowerCase()) {
-      resultado.innerHTML = "<p>¡Respuesta correcta!</p>";
+      contenedorAdivinanzas.innerHTML +=
+        "<p id=texto-resultado>Respuesta correcta!</p>";
+      contenedorAdivinanzas.innerHTML +='<p id=texto-resultado>' + adivinanza.mensaje + '</p>';
+      contenedorAdivinanzas.innerHTML +=
+        "<img src=michi-feliz.jpg width=150px >";
+      contenedorAdivinanzas.innerHTML += "<button id=siguiente>Siguiente</button>";
+      const botonSiguiente = contenedorAdivinanzas.querySelector("#siguiente");
+      botonSiguiente.addEventListener("click", siguienteAdivinanza);
+      const botonEnviar = contenedorAdivinanzas.querySelector("#botonEnviar");
+      botonEnviar.style.display = "none";
+      const input = contenedorAdivinanzas.querySelector("input");
+      input.style.display = "none";
+      const p = contenedorAdivinanzas.querySelector("p");
+      p.style.display = "none";
+
     } else {
-      resultado.innerHTML = "<p>Respuesta incorrecta, inténtalo de nuevo.</p>";
+      contenedorAdivinanzas.innerHTML +=
+        "<p id=texto-resultado>Respuesta incorrecta, inténtalo de nuevo.</p>";
+      contenedorAdivinanzas.innerHTML +=
+        "<img src=michi-triste.jpg width=150px >";
+      const botonEnviar = contenedorAdivinanzas.querySelector("#botonEnviar");
+      botonEnviar.innerHTML = "Intenta nuevamente";
+      const input = contenedorAdivinanzas.querySelector("input");
+      input.style.display = "none";
+      const p = contenedorAdivinanzas.querySelector("p");
+      p.style.display = "none";
     }
-    setTimeout(function () {
-      resultado.innerHTML = "";
-      siguienteAdivinanza();
-    }, 2000);
   });
 }
 
@@ -80,7 +99,7 @@ function siguienteAdivinanza() {
     indiceAdivinanzaActual++;
   } else {
     contenedorAdivinanzas.innerHTML =
-      "<p>¡Todas las adivinanzas han sido resueltas!</p>";
+      "<p>¡Todas las preguntas han sido resueltas!</p>";
   }
 }
 
